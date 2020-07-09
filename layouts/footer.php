ef0b9646eab05784b7a679fa213bee91a8da7e5b
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2020-07-10 02:47:00 +0700

    slice html

---
+++ layouts/footer.php
@@ -0,0 +1,13 @@
+</div>
+
+<footer>
+    <p class="text-center">Made with ❤ by <a href="//github.com/retry19/certificate-name-giver" target="_blank" class="author">re_try</a></p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
+<script src="assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ layouts/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <title>Certificate Name Giver</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row pt-5 justify-content-center">
+            <div class="col-md-4">
+                <h2 class="mb-5">Certificate Name Giver</h2>
+            </div>
+        </div>
