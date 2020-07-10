75984eeb96b4cc6d9e82498d208947a5a026ad45
Reza Rachmanuddin <rezarahmanudin@gmail.com>
2020-07-10 17:07:37 +0700

    download zip feature

--- pdf.php
+++ pdf.php
@@ -15,15 +15,50 @@ $imaegType = pathinfo($image, PATHINFO_EXTENSION);
 $imageData = file_get_contents($image);
 $imageBase64 = 'data:image/' . $imaegType . ';base64,' . base64_encode($imageData);
 
-// var_dump(count($names));
+// var_dump(time());
 // die;
 
-$pdf = new FPDF('L');
-for ($i = 0; $i < count($names); $i++) {
-    $pdf->AddPage();
-    $pdf->Image($imageBase64, 0, 0, 0, 210, 'jpg');
-    $pdf->SetFont('Arial', '', $fontSize + 12);
-    $pdf->SetXY(10, 210 * $yAxis / 100);
-    $pdf->Cell(0, 0, $names[$i], 0, 0, 'C');
+// $pdf = new FPDF('L');
+// for ($i = 0; $i < count($names); $i++) {
+//     $pdf->AddPage();
+//     $pdf->Image($imageBase64, 0, 0, 0, 210, 'jpg');
+//     $pdf->SetFont('Arial', '', $fontSize + 12);
+//     $pdf->SetXY(10, 210 * $yAxis / 100);
+//     $pdf->Cell(0, 0, $names[$i], 0, 0, 'C');
+// }
+// $pdf->Output('', 'reza.pdf');
+$zipName = 'certificate-by-re_try.zip';
+$zipTmp = 'tmp/' . $zipName;
+
+$zip = new ZipArchive();
+
+if ($zip->open($zipTmp, ZipArchive::OVERWRITE | ZipArchive::CREATE)) {
+    foreach ($names as $name) {
+        $pdf = new FPDF('L');
+        $pdf->AddPage();
+        $pdf->Image($imageBase64, 0, 0, 0, 210, 'jpg');
+        $pdf->SetFont('Arial', '', $fontSize + 12);
+        $pdf->SetXY(10, 210 * $yAxis / 100);
+        $pdf->Cell(0, 0, $name, 0, 0, 'C');
+        $content = $pdf->Output('F', 'tmp/' . $name . '.pdf');
+        $zip->addFile('tmp/' . $name . '.pdf', $name . '.pdf');
+    }
+} else {
+    echo "cant open!!";
+    exit;
 }
-$pdf->Output('', 'reza.pdf');
+
+if ($zip->close() === false) {
+    exit("Error creating ZIP file");
+}
+
+if (file_exists($zipTmp)) {
+    header('Content-Type: application/zip');
+    header('Content-Disposition: attachment; filename=' . $zipName);
+    header('Content-length: ' . filesize($zipTmp));
+    header('Pragma: no-cache');
+    header('Expires: 0');
+    readfile($zipTmp);
+} else {
+    exit('Could not find zip file to download!');
+}
